4231198c72faaaacdb00adce9838c8235eaadadf
Shawon Talukder <shawontalukder.study@gmail.com>
2023-02-10 16:53:22 +0600

    allowed origin

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -13,7 +13,7 @@ const allowedOrigin = require("./allowedOrigin");
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigin.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
